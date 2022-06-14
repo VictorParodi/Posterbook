caafd58d1c10e4908f08ce30292d68c88934d603
Victor Parodi <victorparodi@Victors-MacBook-Pro.local>
2022-06-14 14:44:01 -0500

    [development] patching posts 100%

--- src/screens/post/PostScreen.js
+++ src/screens/post/PostScreen.js
@@ -1,51 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
 import Paper from '@mui/material/Paper';
-import { updateData } from '../../helpers/updateData';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
 const PostScreen = () => {
-	const [open, setOpen] = useState(false);
 	const [comments, setComments] = useState([]);
 	const params = useParams();
 	const id = parseInt(params.id);
-	const getPost = useFetch(`/posts/${id}`);
-	const [post, setPost] = useState(getPost);
+	const post = useFetch(`/posts/${id}`);
 	const newComments = useFetch(`/posts/${id}/comments`);
-	let newPost = {};
 
 	useEffect(() => {
-		console.log('HERE !!!');
 		setComments(newComments);
 	}, [post, newComments]);
-
-	const handleOpen = () => setOpen(true);
-  const handleClose = () => {
-		newPost = {
-			id: post.id,
-			title: `${post.id} !!!???`,
-			body:post.body,
-			userId: post.userId
-		}
-		updateData(newPost, `/posts/${post.id}`);
-		setPost(newPost);
-		setOpen(false);
-	};
 
 	const styles = {
 		body: {
@@ -60,8 +27,6 @@ const PostScreen = () => {
 			<Paper style={styles.body}>
 				<p>{post.body}</p>
 			</Paper>
-
-			<Button onClick={handleOpen}>Edit post</Button>
 
 			<Paper style={styles.body}>
 				<h2>Comments</h2>
@@ -78,17 +43,6 @@ const PostScreen = () => {
 					})
 				}
 			</Paper>
-
-			<Modal
-					open={open}
-					onClose={handleClose}
-					aria-labelledby="modal-modal-title"
-					aria-describedby="modal-modal-description"
-				>
-				<Box sx={style}>
-					{post.title}
-				</Box>
-			</Modal>
 		</>
 	);
 }
--- src/ui/row/CustomRow.js
+++ src/ui/row/CustomRow.js
@@ -1,15 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef} from 'react';
 import { useNavigate } from 'react-router-dom';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Modal from '@mui/material/Modal';
 import { red } from '@mui/material/colors';
 import { PostsContext } from '../../context/PostsContext';
 import { deleteData } from '../../helpers/deleteData';
+import { updateData } from '../../helpers/updateData';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const CustomRow = ({row}) => {
+	const [inputTitle, setInputTitle] = useState(row.title)
+	const [open, setOpen] = useState(false);
 	const { posts, setPosts } = useContext(PostsContext);
 	const navigate = useNavigate();
 
@@ -23,42 +41,98 @@ const CustomRow = ({row}) => {
 		setPosts(newPosts);
 	}
 
+	const handleOpen = (event) => {
+		setOpen(true);
+	};
+
+  const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleInputChange = (event) => {
+		setInputTitle(event.target.value);
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const newPost = posts.find(post => post.id === row.id);
+		newPost.title = inputTitle;
+
+		const updatedPosts = posts.map(post => {
+			if (post.id === row.id) { return newPost };
+			return post;
+		});
+
+		updateData(newPost, `/posts/${row.id}`);
+		setPosts(updatedPosts);
+		handleClose();
+	}
+
 	const styles = {
 		icons: { cursor: 'pointer' }
 	}
 
 	return (
-		<TableRow>
-			<TableCell>{row.id}</TableCell>
-			<TableCell>{row.title}</TableCell>
+		<>
+			<TableRow>
+				<TableCell>{row.id}</TableCell>
+				<TableCell>{row.title}</TableCell>
 
-			<TableCell align="right">
-				<VisibilityIcon
-					style={styles.icons}
-					key="view"
-					color="primary"
-					onClick={ handleView }
-				/>
-			</TableCell>
+				<TableCell align="right">
+					<VisibilityIcon
+						style={styles.icons}
+						key="view"
+						color="primary"
+						onClick={ handleView }
+					/>
+				</TableCell>
 
-			<TableCell align="right">
-				<EditIcon
-					style={styles.icons}
-					key="edit"
-					color="success"
-					onClick={ () => console.log('Edit ', row.id) }
-				/>
-			</TableCell>
+				<TableCell align="right">
+					<EditIcon
+						style={styles.icons}
+						key="edit"
+						color="success"
+						onClick={ handleOpen }
+					/>
+				</TableCell>
 
-			<TableCell align="right">
-				<DeleteIcon
-					style={styles.icons}
-					key="del"
-					sx={{ color: red[500] }}
-					onClick={ handleDelete }
-				/>
-			</TableCell>
-		</TableRow>
+				<TableCell align="right">
+					<DeleteIcon
+						style={styles.icons}
+						key="del"
+						sx={{ color: red[500] }}
+						onClick={ handleDelete }
+					/>
+				</TableCell>
+			</TableRow>
+
+			<Modal
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description"
+				>
+				<Box sx={style}>
+					<Box
+						onSubmit={ handleSubmit }
+						component="form"
+						sx={{'& > :not(style)': { m: 1, width: '25ch' }}}
+						noValidate
+						autoComplete="off"
+					>
+						<TextField
+							value={inputTitle}
+							id="outlined-basic"
+							label="Title"
+							variant="outlined"
+							onChange={ handleInputChange}
+						/>
+						{/* <TextField id="filled-basic" label="Body" variant="outlined" /> */}
+						{/* <TextField id="standard-basic" label="Standard" variant="standard" /> */}
+					</Box>
+				</Box>
+			</Modal>
+		</>
 	);
 }
 
